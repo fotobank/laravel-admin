2668ae5551ac484ce860d59e9ce4442a9a61cd22
Jurii <aleksjurii@gmail.com>
2019-11-26 02:32:17 +0200

    25.11.19 некоторые обновления

---
+++ database/migrations/2019_11_25_230032_add_email_fields_to_admin_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddEmailFieldsToAdminUsersTable extends Migration
+{
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+
+		Schema::table(config('admin.database.users_table'), function (Blueprint $table) {
+
+			$table->string('last_name', 190)->after('name')->nullable()->default(null);
+			$table->string('email', 190)->unique()->nullable()->default(null)->after('last_name');
+			$table->string('phone', 190)->unique()->nullable()->default(null)->after('email');
+			$table->string('is_activated', 5)->nullable()->default(null)->after('avatar');
+			$table->string('reset_password_code', 100)->nullable()->after('is_activated');
+			$table->datetime('last_login_at')->nullable()->after('reset_password_code');
+			$table->string('last_login_ip', 190)->nullable()->after('last_login_at');
+
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	    Schema::dropIfExists(config('admin.database.users_table'));
+    }
+}
